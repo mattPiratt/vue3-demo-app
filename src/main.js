01695be458acca649e9655bfdf54bf69b4e4fe3e
Bartosz Kubek <kubek.bartosz@gmail.com>
2023-12-25 14:44:06 +0100

    - created main pages components and conneting them to routes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import Router from './router.js';
 
-createApp({}).mount('#app');
+const app = createApp(App);
+app.use(Router);
+app.mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoatchesDetail from './pages/coatches/Detail.vue';
+import CoatchesList from './pages/coatches/List.vue';
+import CoatchesRegistration from './pages/coatches/Registration.vue';
+import ContactCoatch from './pages/requests/ContactCoatch.vue';
+import ReceivedRequests from './pages/requests/Received.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coatches' },
-    { path: '/coatches', component: null },
+    { path: '/coatches', component: CoatchesList },
     {
       path: '/coatches:id',
-      component: null,
-      children: [{ path: '/contact', component: null }],
+      component: CoatchesDetail,
+      children: [{ path: '/contact', component: ContactCoatch }],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: CoatchesRegistration },
+    { path: '/requests', component: ReceivedRequests },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
