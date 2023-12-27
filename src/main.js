5d3a1ec8decbc26b345fee848904b6c04f5a3bf5
Bartosz Kubek <kubek.bartosz@gmail.com>
2023-12-27 21:10:09 +0100

    - implemented 3 base components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue';
 import Router from './router.js';
 import Store from './store/index.js';
 
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+
 const app = createApp(App);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
 app.use(Router);
 app.use(Store);
 app.mount('#app');
