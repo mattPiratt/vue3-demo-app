8042bbae5b90e8dcfd624024fb8e2201dcadf3b6
Bartosz Kubek <kubek.bartosz@gmail.com>
2023-12-29 16:10:07 +0100

    - registration form prepared

--- src/pages/coatches/Registration.vue
+++ src/pages/coatches/Registration.vue
@@ -1,1 +1,18 @@
-<template><p>not yet implemented</p></template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coatch now!</h2>
+      <coatch-form></coatch-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoatchForm from '../../components/coatches/Form.vue';
+
+export default {
+  components: {
+    CoatchForm,
+  },
+};
+</script>
