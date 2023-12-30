e7c1b1ea2a853f906790bcf749474f25d39ebfd6
Bartosz Kubek <kubek.bartosz@gmail.com>
2023-12-30 19:11:06 +0100

    - implemented form validation

--- src/components/coatches/Form.vue
+++ src/components/coatches/Form.vue
@@ -1,36 +1,84 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="from-controll">
-      <label for="firstnaem">Firstname</label>
-      <input type="text" id="firstanem" v-model.trim="firstName" />
+    <div class="from-controll" :class="{ invalid: !form.firstName.isValid }">
+      <label for="firstname">Firstname</label>
+      <input
+        type="text"
+        id="firstname"
+        v-model.trim="form.firstName.value"
+        @blur="validateOneField('firstName')"
+      />
+      <p v-if="!form.firstName.isValid">First name must not be empty</p>
     </div>
-    <div class="from-controll">
+    <div class="from-controll" :class="{ invalid: !form.lastName.isValid }">
       <label for="lastname">Lastname</label>
-      <input type="text" id="lastname" v-model.trim="lastName" />
+      <input
+        type="text"
+        id="lastname"
+        v-model.trim="form.lastName.value"
+        @blur="validateOneField('lastName')"
+      />
+      <p v-if="!form.lastName.isValid">Last name must not be empty</p>
     </div>
-    <div class="from-controll">
+    <div class="from-controll" :class="{ invalid: !form.description.isValid }">
       <label for="description">Description</label>
-      <textarea id="description" rows="5" v-model.trim="description"></textarea>
+      <textarea
+        id="description"
+        rows="5"
+        v-model.trim="form.description.value"
+        @blur="validateOneField('description')"
+      ></textarea>
+      <p v-if="!form.description.isValid">Description must not be empty</p>
     </div>
-    <div class="from-controll">
+    <div class="from-controll" :class="{ invalid: !form.hourlyRate.isValid }">
       <label for="hourlyRate">Hourly rate</label>
-      <input type="number" id="hourlyRate" v-model.trim="hourlyRate" />
+      <input
+        type="number"
+        id="hourlyRate"
+        v-model.trim="form.hourlyRate.value"
+        @blur="validateOneField('hourlyRate')"
+      />
+      <p v-if="!form.hourlyRate.isValid">
+        Hourly rate must be a positive number
+      </p>
     </div>
-    <div class="from-controll">
+    <div class="from-controll" :class="{ invalid: !form.areas.isValid }">
       <h3>Areas of expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <input
+          type="checkbox"
+          id="frontend"
+          value="frontend"
+          v-model="form.areas.value"
+          @blur="validateOneField('areas')"
+        />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <input
+          type="checkbox"
+          id="backend"
+          value="backend"
+          v-model="form.areas.value"
+          @blur="validateOneField('areas')"
+        />
         <label for="backend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <input
+          type="checkbox"
+          id="career"
+          value="career"
+          v-model="form.areas.value"
+          @blur="validateOneField('areas')"
+        />
         <label for="career">Career Advisory</label>
       </div>
+      <p v-if="!form.areas.isValid">One of those tags must be selected</p>
     </div>
+    <p v-if="!isFormValid">
+      <strong>Please fix above problems and try again.</strong>
+    </p>
     <base-button>Register</base-button>
   </form>
 </template>
@@ -40,23 +88,76 @@ export default {
   emits: ['new-coatch-added'],
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      areas: [],
-      description: '',
-      hourlyRate: null,
+      form: {
+        firstName: {
+          value: '',
+          isValid: true,
+          validator: (self) => {
+            return self.value !== '';
+          },
+        },
+        lastName: {
+          value: '',
+          isValid: true,
+          validator: (self) => {
+            return self.value !== '';
+          },
+        },
+        areas: {
+          value: [],
+          isValid: true,
+          validator: (self) => {
+            return self.value.length > 0;
+          },
+        },
+        description: {
+          value: '',
+          isValid: true,
+          validator: (self) => {
+            return self.value !== '';
+          },
+        },
+        hourlyRate: {
+          value: null,
+          isValid: true,
+          validator: (self) => {
+            return self.value !== null && self.value > 0;
+          },
+        },
+      },
+      isFormValid: true,
     };
   },
   methods: {
     submitForm() {
+      this.validateForm();
+
+      if (this.isFormValid === false) {
+        return false;
+      }
+
       const data = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        areas: this.areas,
-        description: this.description,
-        hourlyRate: this.hourlyRate,
+        firstName: this.form.firstName.value,
+        lastName: this.form.lastName.value,
+        areas: this.form.areas.value,
+        description: this.form.description.value,
+        hourlyRate: this.form.hourlyRate.value,
       };
       this.$emit('new-coatch-added', data);
+    },
+    validateForm() {
+      this.isFormValid = true;
+      Object.keys(this.form).forEach((key) => {
+        this.validateOneField(key);
+      });
+    },
+    validateOneField(fieldName) {
+      this.form[fieldName].isValid = this.form[fieldName].validator(
+        this.form[fieldName]
+      );
+      if (this.form[fieldName].isValid === false) {
+        this.isFormValid = false;
+      }
     },
   },
 };
