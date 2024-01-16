9812c0d3a7ae8db898d8a04ee3f3836238cf6720
Bartosz Kubek <kubek.bartosz@gmail.com>
2024-01-16 15:28:03 +0100

    - not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,1 +1,11 @@
-<template><p>not yet implemented</p></template>
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found!</h2>
+      <p>
+        Maybe check our
+        <router-link :to="{ name: 'coatchesList' }">coatches page!</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
